c5dcd0db2f534eb808c0898247a35783b190ac97
srrthk <sarthakkaushal@outlook.com>
2024-08-27 09:54:00 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Restart-ML
-A repo containing the fundamentals of machine learning
+A repo containing the fundamentals of deep generative & probabilistic modeling. Follows Volodymyr Kuleshov's course on youtube, most models are discussed in the course.
+
+## Directory Structure
+```plaintext
+├── LICENSE
+├── README.md
+├── core
+│   ├── __init__.py
+│   ├── dataLoaders
+│   │   ├── __init__.py
+│   │   └──<dataset name>.py
+│   └── models
+│       └── __init__.py
+├── data
+│   └── <dataset name>
+├── <model name>.ipynb
+└── test_<dataset name>.py
+```
+
+## Data Used for model development:
+- Fashion MNIST
+
+## Deep ML models in repo :
+- Fully Visible Belief Networks
+
+
---
+++ core/dataLoaders/__init__.py
@@ -0,0 +1,40 @@
+#core/dataLoaders/__init__.py
+import os
+class VisionDataTemplate():
+    def __init__(self, 
+                local_dir: str , 
+                batch_size: int
+                ) -> None:
+        self.name = ''
+        self.root_dir = None
+        self.batch_size = None
+    
+    def _is_available_locally(self)->bool:
+        """Checks if data is present in root_dir on local"""
+        data_dir_contents = os.listdir(self.root_dir)
+        if self.name in data_dir_contents:
+            return True
+        return False
+    
+    def _load_data(self):
+        """Downloads data from source if not present in the root_dir on local"""
+        pass
+    
+    def create_loader(self):
+        """Creates sampler over the dataset and provides iterable over the 
+        dataset"""
+        pass
+    
+    def view_sample_stats(self):
+        print('\n------------- Train Stats --------------\n')
+        for X, y in self.train_dataloader:
+            print(f"Shape of X [N, C, H, W]: {X.shape}")
+            print(f"Shape of y: {y.shape} {y.dtype}")
+            break
+        
+        print('\n------------- test Stats  --------------\n')
+        for X, y in self.test_dataloader:
+            print(f"Shape of X [N, C, H, W]: {X.shape}")
+            print(f"Shape of y: {y.shape} {y.dtype}")
+            break
+        print('\n-------------    END      ---------------\n')
