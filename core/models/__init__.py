052dcdc1ba419b1ae4468e7bc65fb2635b8c458a
Sarthakaushal <sarthakkaushal@outlook.com>
2024-08-28 18:45:38 -0400

    ML model creation template

---
+++ core/models/__init__.py
@@ -0,0 +1,12 @@
+from torch import nn
+
+class MlModel(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        # Contains the model architecture 
+        pass
+    
+    def forward(self,x):
+        # x is the input to the model and has to be passed to each layer in the 
+        # ml model
+        pass
