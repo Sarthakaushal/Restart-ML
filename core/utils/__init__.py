ab15df934c411f032df4b384ea6dcf99e38eeae8
Sarthakaushal <sarthakkaushal@outlook.com>
2024-08-28 18:46:34 -0400

    Added utility to select cuda device

---
+++ core/utils/__init__.py
@@ -0,0 +1,14 @@
+# core/utils/__init__.py
+import torch
+
+class Utils():
+    def select_cuda_device():
+        device = (
+            "cuda"
+            if torch.cuda.is_available()
+            else "mps"
+            if torch.backends.mps.is_available()
+            else "cpu"
+        )
+        print(f"Using {device} device")
+        return device
